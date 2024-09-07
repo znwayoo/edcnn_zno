1c3115e83930ab17d30a2b39c5bceeaac284a881
Zarni Nway Oo <znwayooo@gmail.com>
2024-09-07 22:40:28 +0700

    initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from keras.models import load_model
+from keras.preprocessing import image
+import numpy as np
+import cv2
+from PIL import Image
+
+# Load the trained model
+model = load_model('/Users/zno/AIProjects/ModelTesting/streamlit/model/initial_model.keras')
+
+# Define the class labels
+class_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+
+# Function to preprocess the image
+def preprocess_image(img):
+    img = img.resize((48, 48)).convert('L')  # Resize and convert to grayscale
+    img_array = np.array(img)
+    img_array = img_array.astype('float32') / 255  # Normalize
+    img_array = np.expand_dims(img_array, axis=-1)  # Add channel dimension
+    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+    return img_array
+
+# Streamlit app
+st.title('Emotion Detection')
+
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+
+if uploaded_file is not None:
+    # Load 
+    img = Image.open(uploaded_file)
+    
+    # Preprocess the image
+    processed_image = preprocess_image(img)
+    
+    # Make prediction
+    prediction = model.predict(processed_image)
+    emotion = class_labels[np.argmax(prediction)]
+    
+    st.write(f'Predicted Emotion: {emotion}')
+    
+    #Display Image
+    st.image(img, caption='Uploaded Image.', use_column_width=True)
